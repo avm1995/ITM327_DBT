b9674d62b9d0fc6aafc79cdca27a60e7f2c6899f
avm1995 <val19019@byui.edu>
2026-06-20 00:47:03 -0400

    Update staging_news.sql

--- VALERIO_ALLAN_M4/models/staging/staging_news.sql
+++ VALERIO_ALLAN_M4/models/staging/staging_news.sql
@@ -4,15 +4,15 @@ with cleaned as (
 
     select
         cast("id" as number) as news_id,
-        nullif(trim(category), '') as category,
-        to_timestamp_ntz(datetime) as news_datetime,
-        nullif(trim(headline), '') as headline,
-        nullif(trim(image), '') as image_url,
-        nullif(trim(related), '') as related_symbol,
-        nullif(trim(source), '') as news_source,
-        nullif(trim(summary), '') as summary,
-        nullif(trim(url), '') as article_url,
-        try_to_timestamp_ntz(load_date) as load_timestamp
+        nullif(trim("category"), '') as category,
+        to_timestamp_ntz("datetime") as news_datetime,
+        nullif(trim("headline"), '') as headline,
+        nullif(trim("image"), '') as image_url,
+        nullif(trim("related"), '') as related_symbol,
+        nullif(trim("source"), '') as news_source,
+        nullif(trim("summary"), '') as summary,
+        nullif(trim("url"), '') as article_url,
+        try_to_timestamp_ntz("load_date") as load_timestamp
 
     from {{ source('raw', 'valerio_allan_m1') }}
 
